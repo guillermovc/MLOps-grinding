4baf750d985a58434d8cff68e6ee41ef9efef2bf
misazx-1 <misazx@live.com.mx>
2024-04-28 20:58:13 -0700

    agregada libreta eda misael

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from pathlib import Path
 
-main_folder = Path.cwd().parent.parent / 'data' / 'raw'
+raw_folder = Path.cwd(). / 'data' / 'raw'
 
 df = pd.read_parquet(main_folder / 'datos_molienda.parquet')
 
-df.to_parquet( main_folder.parent/processed/'datos_molienda_prueba.parquet')
+df.to_parquet( main_folder.parent/'processed'/'datos_molienda_prueba.parquet')

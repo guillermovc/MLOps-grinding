8ee4ebbd8dfd195696a88bb6c90a0267bce744de
Guillermo Velazquez <guillermo.velc@gmail.com>
2024-04-30 03:45:26 +0000

    pruebas make_dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,8 +1,13 @@
+import os
 import pandas as pd
 from pathlib import Path
 
-main_folder = Path.cwd().parent.parent / 'data' / 'raw'
+print(f"Folder actual: {os.getcwd()}")
+
+main_folder = Path(os.getcwd()) / 'data' / 'raw'
 
 df = pd.read_parquet(main_folder / 'datos_molienda.parquet')
 
-df.to_parquet( main_folder.parent/processed/'datos_molienda_prueba.parquet')
+print(f"Guardando archivo procesado en {main_folder.parent/ 'processed'}")
+
+df.to_parquet( main_folder.parent/ "processed" /'datos_molienda_prueba.parquet')

030dd6819a1a9a42bd0f23b212bb9f325672252e
misazx-1 <misazx@live.com.mx>
2024-05-01 19:28:13 -0700

    agregada libreta de outliers mcd

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,13 +1,33 @@
 import os
 import pandas as pd
 from pathlib import Path
+from pyod.models.mcd import MCD
 
-print(f"Folder actual: {os.getcwd()}")
 
-main_folder = Path(os.getcwd()) / 'data' / 'raw'
+raw_folder = Path(os.getcwd()) / 'data' / 'raw'
+processed_folder = Path(os.getcwd()) / 'data' / 'processed'
 
-df = pd.read_parquet(main_folder / 'datos_molienda.parquet')
 
-print(f"Guardando archivo procesado en {main_folder.parent/ 'processed'}")
 
-df.to_parquet( main_folder.parent/ "processed" /'datos_molienda_prueba.parquet')
+
+#------------------------------------
+#QUITAR OUTLIERS DE df_model.parquet
+
+df = pd.read_parquet(raw_folder / 'df_model.parquet')
+
+mcd_model = MCD(contamination  = 0.01) 
+
+vars_outliers = ['velocidad',
+                'potencia',
+                'flujo_agua',
+                'rendimiento',
+                'presion_ewm']
+                
+mcd_model.fit(df[vars_outliers])
+
+df['outlier'] = mcd_model.labels_
+
+df_no = df[df.outlier == 0].drop(columns = ['outlier'])
+df_no.to_parquet(processed_folder / 'df_model_no_outliers.parquet')
+
+
